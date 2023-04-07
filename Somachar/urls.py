33de9d7a26c9926d48ca8b1ae9a86d007ac3b8a8
mhasan502 <mhasan502@gmail.com>
2023-04-07 23:01:09 +0600

    ui update on header.html & index.html

--- Somachar/urls.py
+++ Somachar/urls.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 from django.urls import include, re_path
 from .views import IndexView
 
-# URLS ########## //TODO
-from .views import EditIndex
-# URLS ##########
-
 # Social Login Urls
 social_auth_patterns = ([
     re_path(r'^accounts/', include('allauth.urls')),
@@ -15,12 +11,6 @@ social_auth_patterns = ([
 # Admin Urls
 admin_pattern = ([
     re_path(r'^admin/', admin.site.urls),
-])
-
-
-# UI Update Urls //TODO
-ui_edit_pattern = ([
-    re_path(r'^edit/', EditIndex),
 ])
 
 urlpatterns = [
@@ -33,8 +23,5 @@ urlpatterns = [
     # Other Patterns
     re_path(r'', include(social_auth_patterns)),
     re_path(r'', include(admin_pattern)),
-
-    # UI Update Urls //TODO
-    re_path(r'', include(ui_edit_pattern)),
 
 ]
